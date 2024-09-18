fe1937e55548490d5dbfa22aa764850696202881
francescodela <franklin.hernandez@correounivalle.edu.co>
2024-09-18 13:08:05 -0500

    Update ejer6parcial2.py

--- ejer6parcial2.py
+++ ejer6parcial2.py
@@ -1,24 +1,38 @@
-class Persona :
-    def __init__ ( self , nombre, edad ):
-        self.__nombre = nombre 
-        self.__edad = edad 
-        
-    def getdelnombre (self):
+
+class Persona:
+    def __init__(self, nombre, edad):
+        self.__nombre = nombre
+        self.__edad = edad
+
+    def get_nombre(self):
         return self.__nombre
-    def setnombre (self, nombre ):
-        self.__nombre= nombre 
-        
-    def getedad (self):
+
+    def set_nombre(self, nombre):
+        self.__nombre = nombre
+
+    def get_edad(self):
         return self.__edad
-    def setedad (self, edad ):
-        if edad >= 0 :
-            self.__edad= edad 
-        else :
-            print ("la edad no puede ser negativa")
-persona = Persona ( "frank", 18)
 
-print  (persona.get__nombre ()) 
-print  (persona.get__edad ()) 
+    def set_edad(self, edad):
+        if edad >= 0:
+            self.__edad = edad
+        else:
+            print("La edad no puede ser negativa")
 
-persona.set__nombre ("frank ")
-persona.set__edad (18)
+
+persona = Persona("Frank", 18)
+
+
+print(persona.get_nombre())  
+print(persona.get_edad())    
+
+
+persona.set_nombre("Frank ")
+persona.set_edad(18)
+
+
+persona.set_edad(-5) 
+
+
+print(persona.get_nombre())  
+print(persona.get_edad())    
